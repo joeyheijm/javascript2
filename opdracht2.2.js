fa5df5012b4cf40448dcb33aff7c23efe965d0a2
joey <joey.heijm@student.rocvf.nl>
2018-11-21 14:28:26 +0100

    opdracht java script

---
+++ opdracht2.2.js
@@ -0,0 +1,36 @@
+bl1.style.backgroundColor = "cyan";
+bl2.style.backgroundColor = "green";
+bl3.style.backgroundColor = "yellow";
+bl4.style.backgroundColor = "red";
+
+bl1.style.height = "100px";
+bl1.style.width = "100px";
+bl1.style.cssFloat = "left";
+
+bl2.style.height = "100px";
+bl2.style.width = "100px";
+bl2.style.cssFloat = "left";
+
+bl3.style.height = "100px";
+bl3.style.width = "100px";
+bl3.style.cssFloat = "left";
+
+bl4.style.height = "100px";
+bl4.style.width = "100px";
+bl4.style.cssFloat = "left";
+
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.textAlign = "right";
+document.getElementById("bl4").style.textAlign = "left";
+
+document.getElementById("bl1").style.lineHeight = "65px";
+
+document.getElementById("bl2").style.position = "relative";
+document.getElementById("bl2").style.top = "20px";
+document.getElementById("bl3").style.position = "relative";
+document.getElementById("bl3").style.top = "40px";
+
+document.body.style.padding = "0px";
+document.body.style.margin = "0px";
+document.body.style.backgroundColor = "aliceblue";
---
+++ opdracht2.js
@@ -0,0 +1,5 @@
+document.getElementById("paginatitel").innerText = "mijn geweldige pagina";
+document.getElementById("subkop").innerText = "mijn geweldige subkop";
+
+document.getElementById("grasparkiet1").src = "parkiet/grasparkiet.jpg";
+document.getElementById("grasparkiet2").src = "parkiet/grasparkiet2.jpg";
